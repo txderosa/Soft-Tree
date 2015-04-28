521ea4eff546f1b6ed6160ec8dfa1e42e2f4cccf
YuHeng <yuhchen21@gmail.com>
2015-04-28 16:24:34 -0400

    Implemented Kruskal's algorithm - implemented DisjointComps class for Kruskal - modified MST algorithms to take in a graph parameter to store MST - added assignment operator to Graph - updated Driver to reflect modifications to MST - tweaked sample_in.txt to test Kruskal - updated Makefile to include DisjointComps

--- Driver.cpp
+++ Driver.cpp
@@ -55,33 +55,56 @@ int main(int argc, char **argv){
       graph.showAdj(false);
     }
 
-    /*
+    
     // run algorithms and time
-    Timer kruskal_timer();
-    kruskal_timer.start();
-    Kruskal(graph);
-    kruskal_timer.end();
-
+    Graph kruskal_mst(nVertices);
+    //Timer kruskal_timer();
+    //kruskal_timer.start();
+    Kruskal(graph, kruskal_mst);
+    //kruskal_timer.end();
+    
+    /*
+    Graph prim_mst(nVertices);
     Timer prim_timer();
     prim_timer.start();
-    Prim(graph);
+    Prim(graph, prim_mst);
     prim_timer.end();
 
+    Graph soft_mst(nVertices);
     Timer soft_timer();
     soft_timer.start();
-    Soft(graph);
+    Soft(graph, soft_mst);
     soft_timer.end();
     */
 
     // report timings
     fout << "Graph " << i+1 << ": " << nVertices << " vertices, " << nEdges << " edges" << std::endl;
+
     fout << "Kruskal: " << 0.0 << std::endl;
-    // print out MST for debugging
+    //kruskal_mst.printEdges(fout);
+    if(DEBUG){
+      std::cerr << "Kruskal MST:" << std::endl;
+      kruskal_mst.showEdges();
+    }
+
     fout << "Prim: " << 0.0 << std::endl;
-    // print out MST for debugging
+    //prim_mst.printEdges(fout);
+    /*
+    if(DEBUG){
+      std::cerr << "Prim MST:" << std::endl;
+      prim_mst.showEdges();
+    }
+    */
+
     fout << "Soft Heap: " << 0.0 << std::endl;
-    // print out MST for debugging
-    
+    //soft_mst.printEdges(fout);
+    /*
+    if(DEBUG){
+      std::cerr << "Soft MST:" << std::endl;
+      soft_mst.showEdges();
+    }
+    */
+
   }
   
   return 0;
--- Graph.h
+++ Graph.h
@@ -22,6 +22,8 @@ class Graph{
  public:
   Graph();
   Graph(int v);
+  
+  Graph& operator=(const Graph &g);
 
   int insertEdge(Edge e);
   int insertEdge(const int v1, const int v2, const int w);
@@ -39,10 +41,13 @@ class Graph{
 
   std::vector<Edge> edges(void);
 
-  void showAdj(bool verbose) const;
-  void showEdges(void) const;
+  void printEdges(std::ostream &out);
 
   ~Graph();
+
+  /*** for debugging ***/
+  void showAdj(bool verbose) const;
+  void showEdges(void) const;
 
 }; // end of class Graph
 
